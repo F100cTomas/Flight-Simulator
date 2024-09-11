4dc0143fafc365d90ee21bfabb2cee5b3a2d30f4
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2024-09-11 20:13:49 +0200

    Spitfire

---
+++ Scripts/airplane.gd
@@ -0,0 +1,13 @@
+extends CharacterBody3D
+
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	velocity += global_rotation * SPEED
+	move_and_slide()
--- Scripts/rigid_body_3d.gd
+++
@@ -1,7 +0,0 @@
-extends RigidBody3D
-
-
-func _input(event: InputEvent) -> void:
-	if event is InputEventKey and event.pressed and event.keycode == KEY_SPACE:
-		linear_velocity.y = 5.0
-		linear_velocity.z = 1.0
