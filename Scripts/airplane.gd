0d47fbe8687b94ccd853a7b7547c1b7badbfb972
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2024-09-12 19:19:50 +0200

    Few changes

--- Scripts/airplane.gd
+++ Scripts/airplane.gd
@@ -1,13 +1,22 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+const SPEED = 20.0
 const JUMP_VELOCITY = 4.5
 
+func rot(delta: float) -> void:
+	rotate(global_basis.x, Input.get_axis("Pitch Down", "Pitch Up") * delta)
+	rotate(global_basis.y, Input.get_axis("Yaw Right", "Yaw Left") * delta)
+	rotate(global_basis.z, Input.get_axis("Roll Clockwise", "Roll Counter-Clockwise") * delta)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
+	rot(delta)
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	velocity += global_rotation * SPEED
+	else:
+		velocity *= 0.9
+	if Input.is_key_pressed(KEY_SPACE):
+		var direction: Vector3 = -global_basis.z.normalized() + global_basis.y.normalized() / 2
+		velocity += direction * delta * SPEED
+	velocity *= 0.99
 	move_and_slide()
