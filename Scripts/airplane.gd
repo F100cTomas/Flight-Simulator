b32428317261bb808f987ffcec696c675a3d9365
F100cTomas <145491617+F100cTomas@users.noreply.github.com>
2024-09-14 19:20:31 +0200

    Boom

--- Scripts/airplane.gd
+++ Scripts/airplane.gd
@@ -1,8 +1,36 @@
 extends CharacterBody3D
 
+const EXPLOSION = preload("res://Assets/Sounds/vine-boom.mp3")
 
 const SPEED = 20.0
 const JUMP_VELOCITY = 4.5
+
+func _ready() -> void:
+	$DuchKieva1/AnimationPlayer.play("Krychle_001Action")
+
+func get_particles() -> GPUParticles3D:
+	var particles: GPUParticles3D = GPUParticles3D.new()
+	particles.amount = 10
+	particles.lifetime = 0.3
+	particles.draw_pass_1 = SphereMesh.new()
+	var material: ParticleProcessMaterial = ParticleProcessMaterial.new()
+	material.emission_shape = ParticleProcessMaterial.EMISSION_SHAPE_SPHERE
+	material.emission_sphere_radius = 1.0
+	material.gravity = Vector3(0, -10.0, 0)
+	material.initial_velocity = Vector2(0.0, 30.0)
+	material.direction = Vector3(0.0, 1.0, 0.0)
+	material.spread = 360.0
+	var gradient: Gradient = Gradient.new()
+	gradient.add_point(0.0, Color(0.5, 0.5, 0.5, 1.0))
+	gradient.add_point(1.0, Color(1.0, 0.5, 0.5, 0.0))
+	var gradient_texture: GradientTexture2D = GradientTexture2D.new()
+	gradient_texture.gradient = gradient
+	material.color_ramp = gradient_texture
+	material.color = Color(0.5, 0.5, 0.5)
+	material.scale = Vector2(0.5, 0.5)
+	particles.process_material = material
+	particles.emitting = true
+	return particles
 
 func rot(delta: float) -> void:
 	rotate(global_basis.x, Input.get_axis("Pitch Down", "Pitch Up") * delta)
@@ -19,4 +47,25 @@ func _physics_process(delta: float) -> void:
 		var direction: Vector3 = -global_basis.z.normalized() + global_basis.y.normalized() / 2
 		velocity += direction * delta * SPEED
 	velocity *= 0.99
-	move_and_slide()
+	if move_and_slide():
+		for i in get_slide_collision_count():
+			var body = get_slide_collision(i).get_collider()
+			if body is RigidBody3D:
+				for b in body.get_colliding_bodies():
+					b.wake_up()
+				var particles : GPUParticles3D = get_particles()
+				particles.position = body.position
+				body.add_sibling(particles)
+				body.queue_free()
+				var timer: Timer = Timer.new()
+				timer.wait_time = 0.5
+				timer.one_shot = true
+				var timeout = func() -> void:
+					particles.queue_free()
+				timer.connect("timeout", timeout)
+				particles.add_child(timer)
+				var audio_player: AudioStreamPlayer3D = AudioStreamPlayer3D.new()
+				audio_player.stream = EXPLOSION
+				particles.add_child(audio_player)
+				timer.start()
+				audio_player.play()
